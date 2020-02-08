0946518141ce29ecf20dba7f0abdd3df007cca93
felri <felipercmelo@gmail.com>
2020-02-08 15:41:40 -0300

    refresh working

--- src/components/Refresh/index.js
+++ src/components/Refresh/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
-import FavoriteOn from 'src/assets/icon/favoriteOn.png'
-import FavoriteOff from 'src/assets/icon/favoriteOff.png'
+import RefreshIcon from 'src/assets/icon/refresh.png'
 import styles from './styles'
 
-export default ({ favorite, toogleFavorite }) => (
-  <TouchableOpacity style={styles.container} onPress={toogleFavorite}>
+export default ({ refresh }) => (
+  <TouchableOpacity style={styles.container} onPress={refresh}>
     <Image
       style={styles.image}
-      source={favorite ? FavoriteOn : FavoriteOff}
+      source={RefreshIcon}
     />
   </TouchableOpacity>
 )
